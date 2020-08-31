ed3750cc511159902e4c2e84aecbef456c895f15
Arina <arianna.gromova@gmail.com>
2020-08-31 16:34:20 +0300

    Log death.py

--- death.py
+++ death.py
@@ -2,6 +2,9 @@
 
 import datetime
 import random
+import contextlib
+from contextlib import ExitStack
+import os
 
 average_life_expectancy = (65, 80)
 current_year = datetime.datetime.now().year
@@ -16,7 +19,6 @@ def death_prediction(age):
         print("Aren't you dead yet?")
         return
 
-    print(death_age)
     death_year = current_year + (death_age - age)
     print(f"Current year: {current_year}\nYou'll die in {death_year}")
 
@@ -24,3 +26,13 @@ def death_prediction(age):
 name = str(input("Your name is: "))
 age = int(input("Your age is: "))
 death_prediction(age)
+
+permission = os.getenv("PERM_LOG", False)
+if permission:
+    with ExitStack() as stack:
+        f = stack.enter_context(open('/tmp/death.log', 'w'))
+        stack.enter_context(contextlib.redirect_stdout(f))
+        print(f"You are {name} and you are {age} years old.")
+        death_prediction(age)
+else:
+    print("You are not permitted to write a log file.")
