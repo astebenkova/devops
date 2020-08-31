b36c011782b8834e3f0a3fbc7256aea020c1bc7a
Arina <arianna.gromova@gmail.com>
2020-08-31 12:04:56 +0300

    Create death.py

--- death.py
+++ death.py
@@ -1,18 +1,26 @@
-#! /usr/local/env python3
+#!/usr/bin/env python3
 
 import datetime
 import random
 
 average_life_expectancy = (65, 80)
-
-# def death_prediction(age):
-#     if age < 25 :
+current_year = datetime.datetime.now().year
 
 
-now = datetime.datetime.now()
+def death_prediction(age):
+    if age < average_life_expectancy[0]:
+        death_age = random.choice(average_life_expectancy)
+    elif age in range(*average_life_expectancy):
+        death_age = random.randint(age, average_life_expectancy[1])
+    else:
+        print("Aren't you dead yet?")
+        return
+
+    print(death_age)
+    death_year = current_year + (death_age - age)
+    print(f"Current year: {current_year}\nYou'll die in {death_year}")
+
+
 name = str(input("Your name is: "))
 age = int(input("Your age is: "))
-# sex =
-death_year = random.randint(now.year+10, now.year+35)
-
-print(f"Current year: {now.year}\nYou'll die in {death_year}")
+death_prediction(age)
