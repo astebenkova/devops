2f82be06d9007b6d32d6a4840d343d5914bbe990
Arina <astebenkova@mirantis.com>
2020-09-08 17:51:51 +0300

    Add some ammendments to proceed script

--- kvm_scripts/vm_preseed.sh
+++ kvm_scripts/vm_preseed.sh
@@ -3,26 +3,28 @@
 # Pre-defined variables
 MEM_SIZE=512
 VCPUS=1
-DISK_SIZE=5
+DISK_SIZE=7
 # none, spice, vnc
 GRAPHICS=none
 VM_NAME=ubuntu_$(date +"%d%m%Y_%H%M")_preseed
 IMAGE_FILE="/var/lib/libvirt/images/basic_images/ubuntu-16.04.6-server-amd64.iso"
-PRESEED_CONF=/share/preseed.cfg
+#IMAGE_FILE="/var/lib/libvirt/images/basic_images/centos-7-x86_64-minimal.iso"
+PRESEED_CONF=preseed.cfg
+NETWORK="net-dhcp-arina"
 
 virt-install --name ${VM_NAME} \
 	     --vcpus ${VCPUS} \
 	     --memory ${MEM_SIZE} \
 	     --quiet \
 	     --disk size=${DISK_SIZE},bus=virtio,format=qcow2 \
-	     --network network=br1 \
+	     --network network=${NETWORK} \
 	     --boot hd \
 	     --graphics ${GRAPHICS} \
 	     --console pty,target_type=serial \
              --location ${IMAGE_FILE} \
 	     --initrd-inject=${PRESEED_CONF} \
              --extra-args="auto=true priority=critical netcfg/use_autoconfig=true netcfg/disable_dhcp=false \
-             netcfg/get_hostname=ubuntu-preseed network-console/password=instpass network-console/start=true \
+             netcfg/get_hostname=docker network-console/password=instpass network-console/start=true \
              console=ttyS0,115200n8 serial"
 
 
